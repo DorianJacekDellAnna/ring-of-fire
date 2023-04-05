c650c1c8cd5a021ed8a99ef74a5c7f778ded319a
Dorian Jacek Dell'Anna <dellanna.youtube@web.de>
2023-04-05 12:22:48 +0200

    added players and dialog

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  game: Game = new Game;
+  currentCard: string = '';
 
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    this.currentCard = this.game.stack.pop();
+    this.pickCardAnimation = true;
+    console.log(this.game)
+
+    setTimeout(() => {
+      this.game.playedCards.push(this.currentCard);
+      this.pickCardAnimation = false;
+    }, 1000);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+  
+    dialogRef.afterClosed().subscribe((name: string) => {
+      this.game.players.push(name);
+    });
+  }
 }
+
+
+
